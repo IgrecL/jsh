01e417dbdf281e7eb581fef22e3f5500dcba1b40
IgrecL <ngowg5p3v@relay.firefox.com>
2023-01-23 17:18:13 +0100

    Comments

--- jsh.js
+++ jsh.js
@@ -5,16 +5,16 @@ const readline = require('readline');
 const fs = require('fs');
 const { execSync } = require("child_process");
 
-// Gestion des entrées clavier
+// Keyboard interactions
 readline.emitKeypressEvents(process.stdin);
 process.stdin.on('keypress', (ch, key) => {
-    if (key.ctrl && key.name == "p") { // On sort si l'utilisateur fait CTRL+P
-        exit(1);
+    if (key.ctrl && key.name == "p") { // Exiting on CTRL+P
+        exit();
     }
 });
 process.stdin.setRawMode(true);
 
-// Boucle asynchrone pour exécuter le shell en boucle
+// Asynchronous loop
 (async () => {
     for (;;) {
         const input = await prompt.get("jsh");
@@ -22,7 +22,7 @@ process.stdin.setRawMode(true);
     }
 })();
 
-// Interprétation de la commande jsh entrée
+// Interprete the jsh command
 function interpreter(input) {
     const parsedInput = input.split(" ");
     switch (parsedInput[0]) {
@@ -40,12 +40,12 @@ function interpreter(input) {
 	});
 }
 
-// Affiche la liste des commandes disponibles
+// Display the list of available commands
 function help() {
 	console.log("\n   - help : display the list of available commands\n   - run <path> [!] : run a process with its absolute or relative path (you can add the postfixe \"!\" to detach the process)\n   - lp : list running processes by decreasing pid\n   - bing [-k|-p|-c] <pid> : kill, pause or resume a running process\n   - keep <pid> : keep a process alive even after closing jsh\n");
 }
 
-// Exécution d'un programme de chemin donné
+// Run a process with its absolute or relative path
 function run(parsedInput) {
     if (parsedInput.length == 2 || parsedInput.length == 3) {
         if (parsedInput[1].charAt(0) == "/" || parsedInput[1].charAt(0) == ".") {
@@ -55,55 +55,56 @@ function run(parsedInput) {
 				execSync(parsedInput[1]);
 			}
 		} else {
-            console.log("Le chemin doit commencer par \"/\" ou \"./\"");
+            console.log("The path has to start with \"/\" or \"./\".");
         }
 	} else if (parsedInput.length < 2) {
-        console.log("Veuillez indiquer le chemin vers l'exécutable.");
+        console.log("Please enter the path.");
     } else {
-        console.log("Trop d'arguments donnés.");
+        console.log("Too many arguments.");
     }
 }
 
-// Liste des processus en cours
+// List running processes by decreasing pid
 function lp(parsedInput) {
     if (parsedInput.length == 1) {
         exec("ps -A",
         function (error, stdout, stderr) {
-            console.log("\nListe des processus : \n\n" + stdout);
+            console.log("\nProcesses list: \n\n" + stdout);
         });
     } else {
-        console.log("Trop d'arguments. Entrez juste \"lp\".")
+        console.log("Too many arguments. Please use \"lp\".")
     }
 }
 
-// Tuer, mettre en pause et reprendre des processus
+// Kill, pause and resume a running process
 function bing(parsedInput) {
     var suffix = []
     var reponse = []
     switch (parsedInput[1]) {
-        case "-k": suffix = " ";      reponse = "tué";        break;
-        case "-p": suffix = "-STOP "; reponse = "interrompu"; break;
+        case "-k": suffix = " ";      reponse = "killed";        break;
+        case "-p": suffix = "-STOP "; reponse = "interrupted"; break;
         case "-c": suffix = "-CONT "; reponse = "relancé";    break;
         default:
-            console.log("Veuillez entrer \"bing [-k|-p|-c] <processId>\".");
+            console.log("Please use \"bing [-k|-p|-c] <pid>\".");
             break;
     }
     exec("kill " + suffix + parsedInput[2],
     function (error) {
         if (error == null) {
-            console.log("Le processus " + parsedInput[2] + " a été " + reponse + ".");
+            console.log("The process " + parsedInput[2] + " was " + reponse + ".");
         } else {
-            console.log("\n" + parsedInput[2] + " n'est pas un pid valide.");
+            console.log("\n" + parsedInput[2] + " is not a valid pid.");
         }
     });
 }
 
+// Keep a process alive even after closing jsh
 function keep(parsedInput) {
 	if (parsedInput.length < 3) {
 		pid = parsedInput[1];
 		exec("kill -SIGSTOP" + pid);
 		exec("kill -SIGCONT" + pid);
 	} else {
-        console.log("Trop d'arguments. Entrez juste \"keep <pid>\".")
+        console.log("Too many arguments. Please use \"keep <pid>\".")
 	}
 }
