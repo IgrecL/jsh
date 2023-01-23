338241c3ff64dfade55e4f18cde2615d05dc1652
IgrecL <ngowg5p3v@relay.firefox.com>
2023-01-23 15:41:27 +0100

    Essai de navigation dans l'historique

--- jsh.js
+++ jsh.js
@@ -2,17 +2,49 @@ const { exit } = require("process");
 const prompt = require("prompt"); prompt.start();
 const exec = require("child_process").exec;
 const readline = require('readline');
+const fs = require('fs');
+var robot = require("robotjs");
+
+var index = 0
+exec('wc -l < .jshistory', function (err, results) {
+	if (err) throw err;
+    index = Number(results) + 1;
+});
 
 // Gestion des entrées clavier
 readline.emitKeypressEvents(process.stdin);
 process.stdin.on('keypress', (ch, key) => {
     if (key.ctrl && key.name == "p") { // On sort si l'utilisateur fait CTRL+P
         exit()
-    } else if (key.name == "up") { // Navigation parmis les commandes précédentes
-        
-    }
+    } else if (key.name == "up" || key.name == "down") { // Navigation parmis les commandes précédentes
+		exec('wc -l < .jshistory', function (err, results) {
+			if (err) throw err;
+			indexMax = Number(results) + 1;
+		});
+		if (key.name == "up") {
+			if (index > 0) {
+				index--;
+			} 
+		} else if (key.name == "down") {
+			if (index < indexMax) {
+				index++;
+			}
+		}
+		for (i = 0; i < 20; i++) {
+			robot.setKeyboardDelay(1);
+			robot.keyTap('backspace');
+		}
+		fs.readFile('.jshistory', (err, data) => {
+			if (err) throw err;
+			robot.setKeyboardDelay(1);
+			robot.typeString(data.toString().split("\n")[index-1]);
+		})
+	}
 });
 process.stdin.setRawMode(true);
+
+// w = window.open('text.txt');
+// w = print();
 
 // Boucle asynchrone pour exécuter le shell en boucle
 (async () => {
@@ -32,18 +64,25 @@ function interpreter(input) {
         case "keep": keep(parsedInput); break;
         default:                        break;
     }
+	fs.appendFile('.jshistory', input+ "\n", err => {
+	    if (err) {
+			console.error(err);
+	  	}	
+	});
 }
 
 // Exécution d'un programme de chemin donné
 function run(parsedInput) {
     if (parsedInput.length == 2 || parsedInput.length == 3) {
         if (parsedInput[1].charAt(0) == "/") {
-			if (parsedInput[3] == "!") {
+			if (parsedInput[2] == "!") {
+				prefix = "nohup "
 				suffix = " &"
 			} else {
+				prefix = ""
 				suffix = ""
 			}
-			exec(parsedInput[1] + suffix);
+			exec(prefix + parsedInput[1] + suffix);
 		} else {
             console.log("Le chemin doit commencer par \"/\"");
         }
@@ -58,7 +97,7 @@ function run(parsedInput) {
 function lp(parsedInput) {
     if (parsedInput.length == 1) {
         exec("ps -A",
-        function (stdout) {
+        function (error, stdout, stderr) {
             console.log("\nListe des processus : \n\n" + stdout);
         });
     } else {
